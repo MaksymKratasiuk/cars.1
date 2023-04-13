217ff13c9a20dd258c68dcb3375e440b28f8307e
MaksymKratasiuk <makshm1307@gmail.com>
2023-04-13 17:46:04 +0300

    First commit

---
+++ Car.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cars
+{
+    internal class Budget_Car
+    {
+        private string _mark;
+        private string _name;
+        private int _price;
+        private string _fuel;
+        private double _engine_volume;
+        
+        
+        public string Mark
+        {
+            get { return _mark; }
+            set { _mark = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Fuel
+        {
+            get { return _fuel; }
+            set { _fuel = value; }
+        }
+
+        public double EngineVolume
+        {
+            get { return _engine_volume; }
+            set { _engine_volume = value; }
+        }
+
+        public Budget_Car()
+        {
+            _mark = "Deawo";
+            _name = "Lanos";
+            _price = 5000;
+            _fuel = "gas";
+            _engine_volume = 1.5;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Mark: {_mark}, Name: {_name}, Price: {_price}, Fuel: {_fuel}, Engine Volume: {_engine_volume}");
+        }
+    }
+
+    internal class Medium_Car
+    {
+        private string _mark;
+        private string _name;
+        private int _price;
+        private string _fuel;
+        private double _engine_volume;
+
+
+        public string Mark
+        {
+            get { return _mark; }
+            set { _mark = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Fuel
+        {
+            get { return _fuel; }
+            set { _fuel = value; }
+        }
+
+        public double EngineVolume
+        {
+            get { return _engine_volume; }
+            set { _engine_volume = value; }
+        }
+
+        public Medium_Car()
+        {
+            _mark = "Toyota";
+            _name = "Camry";
+            _price = 20000;
+            _fuel = "Dizel";
+            _engine_volume = 1.5;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Mark: {_mark}, Name: {_name}, Price: {_price}, Fuel: {_fuel}, Engine Volume: {_engine_volume}");
+        }
+    }
+    internal class Luxury_Car
+    {
+        private string _mark;
+        private string _name;
+        private int _price;
+        private string _fuel;
+        private double _engine_volume;
+
+
+        public string Mark
+        {
+            get { return _mark; }
+            set { _mark = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Fuel
+        {
+            get { return _fuel; }
+            set { _fuel = value; }
+        }
+
+        public double EngineVolume
+        {
+            get { return _engine_volume; }
+            set { _engine_volume = value; }
+        }
+
+        public Luxury_Car()
+        {
+            _mark = "Buggati";
+            _name = "Chiron";
+            _price = 2500000;
+            _fuel = "Benzin";
+            _engine_volume = 8;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Mark: {_mark}, Name: {_name}, Price: {_price}, Fuel: {_fuel}, Engine Volume: {_engine_volume}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What type of car do you want? (budget/medium/luxury)");
+            string carType = Console.ReadLine().ToLower();
+
+            switch (carType)
+            {
+                case "budget":
+                    Budget_Car budgetCar = new Budget_Car();
+                    budgetCar.Show();
+                    break;
+                case "medium":
+                    Medium_Car mediumCar = new Medium_Car();
+                    mediumCar.Show();
+                    break;
+                case "luxury":
+                    Luxury_Car luxuryCar = new Luxury_Car();
+                    luxuryCar.Show();
+                    break;
+                default:
+                    Console.WriteLine("Invalid car type.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
